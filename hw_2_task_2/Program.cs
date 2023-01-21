08b3e9c9c62a232eb290499ccf386f893587f9e0
Kristina <zkvzkvzkv@yandex.ru>
2023-01-22 00:51:26 +0300

    изменена программа для задачи 2

--- hw_2_task_2/Program.cs
+++ hw_2_task_2/Program.cs
@@ -1,12 +1,17 @@
 ﻿// Напишите программу, которая выводит третью цифру 
 // заданного числа или сообщает, что третьей цифры нет.
 
-Console.WriteLine("Введите трехзначное число number");
+Console.WriteLine("Введите число number");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number / 100 > 0)
-{
-    int ThirdNumber = number % 10;
-    Console.WriteLine(ThirdNumber);
-}
-else
+if (number < 100)
     Console.WriteLine("третьей цифры нет");
+else
+{
+    while (number >= 100)
+    {
+        number = number / 10;
+    }
+
+int ThirdNumber = number % 10;
+Console.WriteLine(ThirdNumber);
+}
