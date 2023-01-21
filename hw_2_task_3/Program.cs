b5ee17503336daf3b99020fc8a0cf961bffaa53b
Kristina <zkvzkvzkv@yandex.ru>
2023-01-22 01:08:15 +0300

    создана программа по условию задачи 3

--- hw_2_task_3/Program.cs
+++ hw_2_task_3/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+
+Console.WriteLine("Номер дня");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day % 7 >= 5)
+    Console.WriteLine("выходной");
+else
+    Console.WriteLine("не выходной");
