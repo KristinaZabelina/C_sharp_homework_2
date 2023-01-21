540a27f6f461c37deea67e48b0a3b09f0e140bee
Kristina <zkvzkvzkv@yandex.ru>
2023-01-21 22:56:22 +0300

    создана программа по условию задачи 2

--- hw_2_task_2/Program.cs
+++ hw_2_task_2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите трехзначное число number");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number / 100 > 0)
+{
+    int ThirdNumber = number % 10;
+    Console.WriteLine(ThirdNumber);
+}
+else
+    Console.WriteLine("третьей цифры нет");
